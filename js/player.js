7a7579bd569431d872f202442ec69d41121831b8
ShauryaKaushik <65485931+ShauryaKaushik@users.noreply.github.com>
2020-09-29 13:41:10 +0530

    Add files via upload

---
+++ js/player.js
@@ -0,0 +1,43 @@
+class Player {
+    constructor() {
+        this.index = null;
+        this.distance = 0;
+        this.name = null;
+        this.score =0;
+        this.secondScore = 0;
+        this.allScore = this.score + this.secondScore;
+    }
+
+    getCount() {
+        var playerCountRef = database.ref('playerCount');
+        playerCountRef.on("value", (data) => {
+            playerCount = data.val();
+        })
+    }
+
+    updateCount(count) {
+        database.ref('/').update({
+            playerCount: count
+        });
+    }
+
+    update() {
+        var playerIndex = "players/player" + this.index;
+        database.ref(playerIndex).set({
+            name: this.name,
+            distance: this.distance,
+            score:this.score,
+            secondScore:this.score,
+            allScore:this.allScore
+        });
+    }
+
+    static getPlayerInfo() {
+        var playerInfoRef = database.ref('players');
+        playerInfoRef.on("value", (data) => {
+            allPlayers = data.val();
+        })
+    }
+
+    
+}
